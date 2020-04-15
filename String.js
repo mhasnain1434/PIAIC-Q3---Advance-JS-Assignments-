c868a973e0aea489a437c9432cc3b2ce53f3b051
Muhammad Hasnain <53166754+mhasnain1434@users.noreply.github.com>
2020-04-16 01:48:43 +0500

    Added Files
    String, Template Literal files uploaded

---
+++ String.js
@@ -0,0 +1,15 @@
+<script>
+// Single Line
+
+var str1 = 'My name is Syed Muhammad Hasnain' +
+' and I love football';
+console.log(str1);
+
+// Multiple Lines
+
+var str2 = "Are you sleeping? Are you sleeping?\nBrother John, Brother John!\nMorning bells are ringing!\nMorning bells are ringing!\nDing, ding, dong.\nDing, ding, dong.";
+undefined
+console.log(str2)
+
+
+</script>
---
+++ Template Literals.js
@@ -0,0 +1,14 @@
+<script>
+const userName = 'Syed Muhammad Hasnain';
+
+const RollNumber = 'Q3-CNC-9788'
+
+let date = '16 April 2020';
+
+const coverLetter = `My Name is ${userName} \nMy roll no. is ${RollNumber} \nThe date of assignment submission is ${date}`;
+
+
+//console.log(userName + coverLetter + date);
+console.log(coverLetter);
+
+</script>
