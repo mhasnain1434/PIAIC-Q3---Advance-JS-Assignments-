3df7cf50d3725f148632a2ab8a3d6b8fa8f670f8
Muhammad Hasnain <53166754+mhasnain1434@users.noreply.github.com>
2020-04-16 02:04:56 +0500

    Update Template Literals.js
    roll number var updated to starting lowercase

--- Template Literals.js
+++ Template Literals.js
@@ -1,12 +1,12 @@
 <script>
 const userName = 'Syed Muhammad Hasnain';
 
-const RollNumber = 'Q3-CNC-9788'
+const rollNumber = 'Q3-CNC-9788';
 
 let date = '16 April 2020';
 
 const coverLetter = `My Name is ${userName}
-My roll no. is ${RollNumber}
+My roll no. is ${rollNumber}
 The date of assignment submission is ${date}`;
 
 //console.log(userName + coverLetter + date);
